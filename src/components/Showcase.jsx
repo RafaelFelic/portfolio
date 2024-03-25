d16d3651f3fb466bbb2fb96dbec38034a3bf76a5
Rafael <rafaelfelic@gmail.com>
2024-03-25 16:28:29 +1000

    Landscape viewport updated 25.03

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,4 +1,4 @@
-import useTextAnimation from '../utils/textAnimation';
+import useTextAnimation from "../utils/textAnimation";
 
 export default function Showcase() {
   const sectionRef = useTextAnimation();
@@ -8,11 +8,11 @@ export default function Showcase() {
       ref={sectionRef}
       className="h-screen flex flex-col justify-evenly xl:p-12 items-center md:flex-row"
     >
-      <section className="block md:inline-block md:m-auto mr-5 md:ml-12 w-full md:w-[60%] text-2xl md:text-3xl lg:text-4xl text-right md:text-left font-semibold landscape">
+      <section className="block md:inline-block md:m-auto mr-5 md:ml-12 w-full md:w-[60%] text-2xl md:text-3xl lg:text-4xl text-right md:text-left font-semibold">
         <div>
           <p className="hide fromleft mb-3 md:mb-8">Welcome!</p>
           <p className="hide fromleft mb-2 md:mb-3">
-            I am{' '}
+            I am{" "}
             <span className="text-[#4169E1] text-3xl md:text-4xl lg:text-5xl">
               Rafael Feliciano
             </span>
@@ -26,9 +26,9 @@ export default function Showcase() {
         </div>
       </section>
 
-      <section className="w-[85%] md:w-[40%] md:mr-12 text-base lg:text-lg bg-[#95AADD] rounded-3xl text-black p-2.5 shadow-[20px_20px_0px_2px_#000] hide fromright">
+      <section className="w-[85%] md:w-[40%] md:mr-12 text-base lg:text-lg bg-[#95AADD] rounded-3xl text-black p-2.5 shadow-[20px_20px_0px_2px_#000] hide fromright landscape">
         <div className="p-2.5 skills">
-          <p className="text-2xl md:text-3xl md:mb-6 ">skills &#123;</p>
+          <p className="text-2xl md:text-3xl mb-0 ">skills &#123;</p>
           <p className="mb-5">
             <span className="font-semibold">profession:</span> Front-End
             Developer,
@@ -40,7 +40,7 @@ export default function Showcase() {
           </p>
         </div>
         <div className="mt-2.5 p-2.5 work">
-          <p className="text-2xl md:text-3xl md:mb-6">work &#123;</p>
+          <p className="text-2xl md:text-3xl mb-0">work &#123;</p>
           <p className="mb-5">
             <span className="font-semibold">currentJob:</span> Freelancer,
           </p>
